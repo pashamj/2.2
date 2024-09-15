15b7c87e9100e45ee5e4c6bc08d5736fbf859aa7
Pavel Mejinschii <pmezhinsky@bk.ru>
2024-09-15 15:07:20 +0300

    final_result

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import styles from './App.module.css';
-import { ErrorComponent } from "./ErrorComponent";
-import {ListContainerComponent} from "./ListContainerComponent"
-import {ButtonContainerComponent} from "./ButtonContainerComponent";
+// import { ErrorComponent } from "./ErrorComponent";
+// import {ListContainerComponent} from "./ListContainerComponent"
+// import {ButtonContainerComponent} from "./ButtonContainerComponent";
 import React, {useState} from "react";
-import {isElementType} from "@testing-library/user-event/dist/utils";
 
 const  App = () =>  {
-
     const validate = (val) => val.length >= 3;
-
 
     const [list ,setList] = useState([])
     const [error, setError] = useState('')
@@ -17,17 +14,14 @@ const  App = () =>  {
         let newValue = prompt("ВВедите значение");
         if(!validate(newValue)){
             setError('Введите больше 2 символов')
-
         }else {
             setError('')
             setValue(newValue)
-
         }
-
     }
     // const updatedList = [...list, { id:Date.now, value }]
     const onAddButtonClick = () => {
-        if (!validate(value)) {
+        if (validate(value)) {
             const updatedList = [...list, { id: Date.now(), value }]
             setList((prev) => [...prev, {id: Date.now(), value, date: new Date()}]);
             setList(updatedList)
@@ -37,39 +31,29 @@ const  App = () =>  {
         }
         return list
     };
-
-
     const isValueValid = validate(value);
-
-
     return (
-        <div className="app">
+        <div className={styles.app}>
             <h1 className="page-heading">Ввод значения</h1>
             <p className="no-margin-text">
                 Текущее значение <code></code>: "
                 <output className="current-value">{value}</output>
                 "
             </p>
-            <div className="error">{error}</div>
-            <div className="buttons-container">
+            <div className={styles.error}>{error}</div>
+            <div className={styles.buttonsContainer}>
                 <button onClick={getNewValue}>Ввести новое</button>
 
                 <button onClick={onAddButtonClick} disabled={!isValueValid}>Добавить в список</button>
             </div>
-            <div className="list-container">
-                <h2 className="list-heading">Список:</h2>
+            <div className={styles.listContainer}>
+                <h2 className={styles.listHeading}>Список:</h2>
                 <p className="no-margin-text">Нет добавленных элементов</p>
                 <ul className="list">
-                    {/*<li className="list-item">Первый элемент</li>*/}
                     {list.map(({id, value})=><li key = {id}>{value}</li>)}
                 </ul>
             </div>
         </div>
-
-
-
-
-
   );
 }
 export default App
